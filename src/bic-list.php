e2a3ed725ad6f3cd6b7c5b6c12365534dee9c254
stefvanesch <stefvanesch@users.noreply.github.com>
2020-10-10 15:21:48 +0000

    Fix styling

--- src/bic-list.php
+++ src/bic-list.php
@@ -1,354 +1,354 @@
 <?php
 
-	return [
-		[
-			'bic' => 'AABNNL2A',
-			'identifier' => 'AABN',
-			'name' => 'ABN AMRO Trading as Kendu'
-		],
-		[
-			'bic' => 'ABNANL2A',
-			'identifier' => 'ABNA',
-			'name' => 'ABN AMRO Bank'
-		],
-		[
-			'bic' => 'ABNANL2A',
-			'identifier' => 'FTSB',
-			'name' => 'ABN AMRO (ex FORTIS)'
-		],
-		[
-			'bic' => 'ABNCNL2A',
-			'identifier' => 'ABNC',
-			'name' => 'ABN AMRO Clearing Bank N.V.'
-		],
-		[
-			'bic' => 'ADYBNL2A',
-			'identifier' => 'ADYB',
-			'name' => 'ADYEN'
-		],
-		[
-			'bic' => 'AEGONL2U',
-			'identifier' => 'AEGO',
-			'name' => 'AEGON BANK'
-		],
-		[
-			'bic' => 'ANDLNL2A',
-			'identifier' => 'ANDL',
-			'name' => 'ANADOLUBANK'
-		],
-		[
-			'bic' => 'ARBNNL22',
-			'identifier' => 'ARBN',
-			'name' => 'ACHMEA BANK'
-		],
-		[
-			'bic' => 'ARSNNL21',
-			'identifier' => 'ARSN',
-			'name' => 'ARGENTA SPAARBANK'
-		],
-		[
-			'bic' => 'ASNBNL21',
-			'identifier' => 'ASNB',
-			'name' => 'ASN BANK'
-		],
-		[
-			'bic' => 'ATBANL2A',
-			'identifier' => 'ATBA',
-			'name' => 'AMSTERDAM TRADE BANK'
-		],
-		[
-			'bic' => 'BCDMNL22',
-			'identifier' => 'BCDM',
-			'name' => 'BANQUE CHAABI DU MAROC'
-		],
-		[
-			'bic' => 'BCITNL2A',
-			'identifier' => 'BCIT',
-			'name' => 'INTESA SANPAOLO'
-		],
-		[
-			'bic' => 'BICKNL2A',
-			'identifier' => 'BICK',
-			'name' => 'BINCKBANK'
-		],
-		[
-			'bic' => 'BINKNL21',
-			'identifier' => 'BINK',
-			'name' => 'BINCKBANK, PROF'
-		],
-		[
-			'bic' => 'BITSNL2A',
-			'identifier' => 'BITS',
-			'name' => 'BITSAFE PAYMENTS'
-		],
-		[
-			'bic' => 'BKCHNL2R',
-			'identifier' => 'BKCH',
-			'name' => 'BANK OF CHINA'
-		],
-		[
-			'bic' => 'BKMGNL2A',
-			'identifier' => 'BKMG',
-			'name' => 'BANK MENDES GANS'
-		],
-		[
-			'bic' => 'BLGWNL21',
-			'identifier' => 'BLGW',
-			'name' => 'BLG WONEN'
-		],
-		[
-			'bic' => 'BMEUNL21',
-			'identifier' => 'BMEU',
-			'name' => 'BMCE EUROSERVICES'
-		],
-		[
-			'bic' => 'BNDANL2A',
-			'identifier' => 'BNDA',
-			'name' => 'BRAND NEW DAY BANK'
-		],
-		[
-			'bic' => 'BNGHNL2G',
-			'identifier' => 'BNGH',
-			'name' => 'BANK NEDERLANDSE GEMEENTEN'
-		],
-		[
-			'bic' => 'BNPANL2A',
-			'identifier' => 'BNPA',
-			'name' => 'BNP PARIBAS'
-		],
-		[
-			'bic' => 'BOFANLNX',
-			'identifier' => 'BOFA',
-			'name' => 'BANK OF AMERICA'
-		],
-		[
-			'bic' => 'BOFSNL21002',
-			'identifier' => 'BOFS',
-			'name' => 'BANK OF SCOTLAND, AMSTERDAM'
-		],
-		[
-			'bic' => 'BOTKNL2X',
-			'identifier' => 'BOTK',
-			'name' => 'MUFG BANK'
-		],
-		[
-			'bic' => 'BUNQNL2A',
-			'identifier' => 'BUNQ',
-			'name' => 'BUNQ'
-		],
-		[
-			'bic' => 'CHASNL2X',
-			'identifier' => 'CHAS',
-			'name' => 'JPMORGAN CHASE'
-		],
-		[
-			'bic' => 'CITCNL2A',
-			'identifier' => 'CITC',
-			'name' => 'CITCO BANK'
-		],
-		[
-			'bic' => 'CITINL2X',
-			'identifier' => 'CITI',
-			'name' => 'CITIBANK INTERNATIONAL'
-		],
-		[
-			'bic' => 'COBANL2X',
-			'identifier' => 'COBA',
-			'name' => 'COMMERZBANK'
-		],
-		[
-			'bic' => 'DEUTNL2A',
-			'identifier' => 'DEUT',
-			'name' => 'DEUTSCHE BANK'
-		],
-		[
-			'bic' => 'DHBNNL2R',
-			'identifier' => 'DHBN',
-			'name' => 'DEMIR-HALK BANK'
-		],
-		[
-			'bic' => 'DLBKNL2A',
-			'identifier' => 'DLBK',
-			'name' => 'DELTA LLOYD BANK'
-		],
-		[
-			'bic' => 'DNIBNL2G',
-			'identifier' => 'DNIB',
-			'name' => 'NIBC'
-		],
-		[
-			'bic' => 'EBURNL21',
-			'identifier' => 'EBUR',
-			'name' => 'EBURY NETHERLANDS'
-		],
-		[
-			'bic' => 'FBHLNL2A',
-			'identifier' => 'FBHL',
-			'name' => 'CREDIT EUROPE BANK'
-		],
-		[
-			'bic' => 'FLORNL2A',
-			'identifier' => 'FLOR',
-			'name' => 'DE NEDERLANDSCHE BANK'
-		],
-		[
-			'bic' => 'FRNXNL2A',
-			'identifier' => 'FRNX',
-			'name' => 'FRANX'
-		],
-		[
-			'bic' => 'FVLBNL22',
-			'identifier' => 'FVLB',
-			'name' => 'VAN LANSCHOT'
-		],
-		[
-			'bic' => 'GILLNL2A',
-			'identifier' => 'GILL',
-			'name' => 'INSINGERGILISSEN'
-		],
-		[
-			'bic' => 'HANDNL2A',
-			'identifier' => 'HAND',
-			'name' => 'SVENSKA HANDELSBANKEN'
-		],
-		[
-			'bic' => 'HHBANL22',
-			'identifier' => 'HHBA',
-			'name' => 'HOF HOORNEMAN BANKIERS'
-		],
-		[
-			'bic' => 'HSBCNL2A',
-			'identifier' => 'HSBC',
-			'name' => 'HSBC BANK'
-		],
-		[
-			'bic' => 'ICBCNL2A',
-			'identifier' => 'ICBC',
-			'name' => 'MEGA INTERNATIONAL COMMERCIAL BANK'
-		],
-		[
-			'bic' => 'ICBKNL2A',
-			'identifier' => 'ICBK',
-			'name' => 'INDUSTRIAL & COMMERCIAL BANK OF CHINA'
-		],
-		[
-			'bic' => 'INGBNL2A',
-			'identifier' => 'INGB',
-			'name' => 'ING'
-		],
-		[
-			'bic' => 'ISAENL2A',
-			'identifier' => 'ISAE',
-			'name' => 'CACEIS BANK, Netherlands Branch'
-		],
-		[
-			'bic' => 'ISBKNL2A',
-			'identifier' => 'ISBK',
-			'name' => 'ISBANK'
-		],
-		[
-			'bic' => 'KABANL2A',
-			'identifier' => 'KABA',
-			'name' => 'YAPI KREDI BANK'
-		],
-		[
-			'bic' => 'KASANL2A',
-			'identifier' => 'KASA',
-			'name' => 'KAS BANK'
-		],
-		[
-			'bic' => 'KNABNL2H',
-			'identifier' => 'KNAB',
-			'name' => 'KNAB'
-		],
-		[
-			'bic' => 'KOEXNL2A',
-			'identifier' => 'KOEX',
-			'name' => 'KEB HANA BANK'
-		],
-		[
-			'bic' => 'KREDNL2X',
-			'identifier' => 'KRED',
-			'name' => 'KBC BANK'
-		],
-		[
-			'bic' => 'LOCYNL2A',
-			'identifier' => 'LOCY',
-			'name' => 'LOMBARD ODIER DARIER HENTSCH & CIE'
-		],
-		[
-			'bic' => 'LOYDNL2A',
-			'identifier' => 'LOYD',
-			'name' => 'LLOYDS TSB BANK'
-		],
-		[
-			'bic' => 'LPLNNL2F',
-			'identifier' => 'LPLN',
-			'name' => 'LEASEPLAN CORPORATION'
-		],
-		[
-			'bic' => 'MHCBNL2A',
-			'identifier' => 'MHCB',
-			'name' => 'MIZUHO BANK EUROPE NV'
-		],
-		[
-			'bic' => 'MOYONL21',
-			'identifier' => 'MOYO',
-			'name' => 'MONEYOU'
-		],
-		[
-			'bic' => 'NNBANL2G',
-			'identifier' => 'NNBA',
-			'name' => 'NATIONALE-NEDERLANDEN BANK'
-		],
-		[
-			'bic' => 'NWABNL2G',
-			'identifier' => 'NWAB',
-			'name' => 'NEDERLANDSE WATERSCHAPSBANK'
-		],
-		[
-			'bic' => 'PCBCNL2A',
-			'identifier' => 'PCBC',
-			'name' => 'CHINA CONSTRUCTION BANK, AMSTERDAM BRANCH'
-		],
-		[
-			'bic' => 'RABONL2U',
-			'identifier' => 'RABO',
-			'name' => 'RABOBANK'
-		],
-		[
-			'bic' => 'RBRBNL21',
-			'identifier' => 'RBRB',
-			'name' => 'REGIOBANK'
-		],
-		[
-			'bic' => 'SNSBNL2A',
-			'identifier' => 'SNSB',
-			'name' => 'SNS'
-		],
-		[
-			'bic' => 'SOGENL2A',
-			'identifier' => 'SOGE',
-			'name' => 'SOCIETE GENERALE'
-		],
-		[
-			'bic' => 'TRIONL2U',
-			'identifier' => 'TRIO',
-			'name' => 'TRIODOS BANK'
-		],
-		[
-			'bic' => 'UGBINL2A',
-			'identifier' => 'UGBI',
-			'name' => 'GARANTIBANK INTERNATIONAL'
-		],
-		[
-			'bic' => 'VOWANL21',
-			'identifier' => 'VOWA',
-			'name' => 'VOLKSWAGEN BANK'
-		],
-		[
-			'bic' => 'ZWLBNL21',
-			'identifier' => 'ZWLB',
-			'name' => 'SNS'
-		],
-	];
+    return [
+        [
+            'bic' => 'AABNNL2A',
+            'identifier' => 'AABN',
+            'name' => 'ABN AMRO Trading as Kendu',
+        ],
+        [
+            'bic' => 'ABNANL2A',
+            'identifier' => 'ABNA',
+            'name' => 'ABN AMRO Bank',
+        ],
+        [
+            'bic' => 'ABNANL2A',
+            'identifier' => 'FTSB',
+            'name' => 'ABN AMRO (ex FORTIS)',
+        ],
+        [
+            'bic' => 'ABNCNL2A',
+            'identifier' => 'ABNC',
+            'name' => 'ABN AMRO Clearing Bank N.V.',
+        ],
+        [
+            'bic' => 'ADYBNL2A',
+            'identifier' => 'ADYB',
+            'name' => 'ADYEN',
+        ],
+        [
+            'bic' => 'AEGONL2U',
+            'identifier' => 'AEGO',
+            'name' => 'AEGON BANK',
+        ],
+        [
+            'bic' => 'ANDLNL2A',
+            'identifier' => 'ANDL',
+            'name' => 'ANADOLUBANK',
+        ],
+        [
+            'bic' => 'ARBNNL22',
+            'identifier' => 'ARBN',
+            'name' => 'ACHMEA BANK',
+        ],
+        [
+            'bic' => 'ARSNNL21',
+            'identifier' => 'ARSN',
+            'name' => 'ARGENTA SPAARBANK',
+        ],
+        [
+            'bic' => 'ASNBNL21',
+            'identifier' => 'ASNB',
+            'name' => 'ASN BANK',
+        ],
+        [
+            'bic' => 'ATBANL2A',
+            'identifier' => 'ATBA',
+            'name' => 'AMSTERDAM TRADE BANK',
+        ],
+        [
+            'bic' => 'BCDMNL22',
+            'identifier' => 'BCDM',
+            'name' => 'BANQUE CHAABI DU MAROC',
+        ],
+        [
+            'bic' => 'BCITNL2A',
+            'identifier' => 'BCIT',
+            'name' => 'INTESA SANPAOLO',
+        ],
+        [
+            'bic' => 'BICKNL2A',
+            'identifier' => 'BICK',
+            'name' => 'BINCKBANK',
+        ],
+        [
+            'bic' => 'BINKNL21',
+            'identifier' => 'BINK',
+            'name' => 'BINCKBANK, PROF',
+        ],
+        [
+            'bic' => 'BITSNL2A',
+            'identifier' => 'BITS',
+            'name' => 'BITSAFE PAYMENTS',
+        ],
+        [
+            'bic' => 'BKCHNL2R',
+            'identifier' => 'BKCH',
+            'name' => 'BANK OF CHINA',
+        ],
+        [
+            'bic' => 'BKMGNL2A',
+            'identifier' => 'BKMG',
+            'name' => 'BANK MENDES GANS',
+        ],
+        [
+            'bic' => 'BLGWNL21',
+            'identifier' => 'BLGW',
+            'name' => 'BLG WONEN',
+        ],
+        [
+            'bic' => 'BMEUNL21',
+            'identifier' => 'BMEU',
+            'name' => 'BMCE EUROSERVICES',
+        ],
+        [
+            'bic' => 'BNDANL2A',
+            'identifier' => 'BNDA',
+            'name' => 'BRAND NEW DAY BANK',
+        ],
+        [
+            'bic' => 'BNGHNL2G',
+            'identifier' => 'BNGH',
+            'name' => 'BANK NEDERLANDSE GEMEENTEN',
+        ],
+        [
+            'bic' => 'BNPANL2A',
+            'identifier' => 'BNPA',
+            'name' => 'BNP PARIBAS',
+        ],
+        [
+            'bic' => 'BOFANLNX',
+            'identifier' => 'BOFA',
+            'name' => 'BANK OF AMERICA',
+        ],
+        [
+            'bic' => 'BOFSNL21002',
+            'identifier' => 'BOFS',
+            'name' => 'BANK OF SCOTLAND, AMSTERDAM',
+        ],
+        [
+            'bic' => 'BOTKNL2X',
+            'identifier' => 'BOTK',
+            'name' => 'MUFG BANK',
+        ],
+        [
+            'bic' => 'BUNQNL2A',
+            'identifier' => 'BUNQ',
+            'name' => 'BUNQ',
+        ],
+        [
+            'bic' => 'CHASNL2X',
+            'identifier' => 'CHAS',
+            'name' => 'JPMORGAN CHASE',
+        ],
+        [
+            'bic' => 'CITCNL2A',
+            'identifier' => 'CITC',
+            'name' => 'CITCO BANK',
+        ],
+        [
+            'bic' => 'CITINL2X',
+            'identifier' => 'CITI',
+            'name' => 'CITIBANK INTERNATIONAL',
+        ],
+        [
+            'bic' => 'COBANL2X',
+            'identifier' => 'COBA',
+            'name' => 'COMMERZBANK',
+        ],
+        [
+            'bic' => 'DEUTNL2A',
+            'identifier' => 'DEUT',
+            'name' => 'DEUTSCHE BANK',
+        ],
+        [
+            'bic' => 'DHBNNL2R',
+            'identifier' => 'DHBN',
+            'name' => 'DEMIR-HALK BANK',
+        ],
+        [
+            'bic' => 'DLBKNL2A',
+            'identifier' => 'DLBK',
+            'name' => 'DELTA LLOYD BANK',
+        ],
+        [
+            'bic' => 'DNIBNL2G',
+            'identifier' => 'DNIB',
+            'name' => 'NIBC',
+        ],
+        [
+            'bic' => 'EBURNL21',
+            'identifier' => 'EBUR',
+            'name' => 'EBURY NETHERLANDS',
+        ],
+        [
+            'bic' => 'FBHLNL2A',
+            'identifier' => 'FBHL',
+            'name' => 'CREDIT EUROPE BANK',
+        ],
+        [
+            'bic' => 'FLORNL2A',
+            'identifier' => 'FLOR',
+            'name' => 'DE NEDERLANDSCHE BANK',
+        ],
+        [
+            'bic' => 'FRNXNL2A',
+            'identifier' => 'FRNX',
+            'name' => 'FRANX',
+        ],
+        [
+            'bic' => 'FVLBNL22',
+            'identifier' => 'FVLB',
+            'name' => 'VAN LANSCHOT',
+        ],
+        [
+            'bic' => 'GILLNL2A',
+            'identifier' => 'GILL',
+            'name' => 'INSINGERGILISSEN',
+        ],
+        [
+            'bic' => 'HANDNL2A',
+            'identifier' => 'HAND',
+            'name' => 'SVENSKA HANDELSBANKEN',
+        ],
+        [
+            'bic' => 'HHBANL22',
+            'identifier' => 'HHBA',
+            'name' => 'HOF HOORNEMAN BANKIERS',
+        ],
+        [
+            'bic' => 'HSBCNL2A',
+            'identifier' => 'HSBC',
+            'name' => 'HSBC BANK',
+        ],
+        [
+            'bic' => 'ICBCNL2A',
+            'identifier' => 'ICBC',
+            'name' => 'MEGA INTERNATIONAL COMMERCIAL BANK',
+        ],
+        [
+            'bic' => 'ICBKNL2A',
+            'identifier' => 'ICBK',
+            'name' => 'INDUSTRIAL & COMMERCIAL BANK OF CHINA',
+        ],
+        [
+            'bic' => 'INGBNL2A',
+            'identifier' => 'INGB',
+            'name' => 'ING',
+        ],
+        [
+            'bic' => 'ISAENL2A',
+            'identifier' => 'ISAE',
+            'name' => 'CACEIS BANK, Netherlands Branch',
+        ],
+        [
+            'bic' => 'ISBKNL2A',
+            'identifier' => 'ISBK',
+            'name' => 'ISBANK',
+        ],
+        [
+            'bic' => 'KABANL2A',
+            'identifier' => 'KABA',
+            'name' => 'YAPI KREDI BANK',
+        ],
+        [
+            'bic' => 'KASANL2A',
+            'identifier' => 'KASA',
+            'name' => 'KAS BANK',
+        ],
+        [
+            'bic' => 'KNABNL2H',
+            'identifier' => 'KNAB',
+            'name' => 'KNAB',
+        ],
+        [
+            'bic' => 'KOEXNL2A',
+            'identifier' => 'KOEX',
+            'name' => 'KEB HANA BANK',
+        ],
+        [
+            'bic' => 'KREDNL2X',
+            'identifier' => 'KRED',
+            'name' => 'KBC BANK',
+        ],
+        [
+            'bic' => 'LOCYNL2A',
+            'identifier' => 'LOCY',
+            'name' => 'LOMBARD ODIER DARIER HENTSCH & CIE',
+        ],
+        [
+            'bic' => 'LOYDNL2A',
+            'identifier' => 'LOYD',
+            'name' => 'LLOYDS TSB BANK',
+        ],
+        [
+            'bic' => 'LPLNNL2F',
+            'identifier' => 'LPLN',
+            'name' => 'LEASEPLAN CORPORATION',
+        ],
+        [
+            'bic' => 'MHCBNL2A',
+            'identifier' => 'MHCB',
+            'name' => 'MIZUHO BANK EUROPE NV',
+        ],
+        [
+            'bic' => 'MOYONL21',
+            'identifier' => 'MOYO',
+            'name' => 'MONEYOU',
+        ],
+        [
+            'bic' => 'NNBANL2G',
+            'identifier' => 'NNBA',
+            'name' => 'NATIONALE-NEDERLANDEN BANK',
+        ],
+        [
+            'bic' => 'NWABNL2G',
+            'identifier' => 'NWAB',
+            'name' => 'NEDERLANDSE WATERSCHAPSBANK',
+        ],
+        [
+            'bic' => 'PCBCNL2A',
+            'identifier' => 'PCBC',
+            'name' => 'CHINA CONSTRUCTION BANK, AMSTERDAM BRANCH',
+        ],
+        [
+            'bic' => 'RABONL2U',
+            'identifier' => 'RABO',
+            'name' => 'RABOBANK',
+        ],
+        [
+            'bic' => 'RBRBNL21',
+            'identifier' => 'RBRB',
+            'name' => 'REGIOBANK',
+        ],
+        [
+            'bic' => 'SNSBNL2A',
+            'identifier' => 'SNSB',
+            'name' => 'SNS',
+        ],
+        [
+            'bic' => 'SOGENL2A',
+            'identifier' => 'SOGE',
+            'name' => 'SOCIETE GENERALE',
+        ],
+        [
+            'bic' => 'TRIONL2U',
+            'identifier' => 'TRIO',
+            'name' => 'TRIODOS BANK',
+        ],
+        [
+            'bic' => 'UGBINL2A',
+            'identifier' => 'UGBI',
+            'name' => 'GARANTIBANK INTERNATIONAL',
+        ],
+        [
+            'bic' => 'VOWANL21',
+            'identifier' => 'VOWA',
+            'name' => 'VOLKSWAGEN BANK',
+        ],
+        [
+            'bic' => 'ZWLBNL21',
+            'identifier' => 'ZWLB',
+            'name' => 'SNS',
+        ],
+    ];
